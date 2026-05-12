25a50998b7fa3b8fae26d192ce2219072270dcfd
DigitalData <legoxavierlocketravers@gmail.com>
2026-05-12 23:59:36 +1000

    Add root I guess

--- modules/common/users.nix
+++ modules/common/users.nix
@@ -4,6 +4,13 @@
   # System-level user definitions
   # Configure user accounts per host
 
+  users.users.root = {
+    initialPassword = "rootpass";
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILk4bC/9jWdrMGhuJTfIVpc+YyEULpFKaGQHIL2sRtV8 octantis"
+    ];
+  };
+
   users.users.digitaldata = {
     isNormalUser = true;
     description = "digitaldata";
