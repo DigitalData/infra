bfc06ca5262ffb502cad1b9b53539f97138f1586
DigitalData <legoxavierlocketravers@gmail.com>
2026-05-12 22:56:34 +1000

    Haiku - Theoretically support SSH login without password

--- modules/common/users.nix
+++ modules/common/users.nix
@@ -9,5 +9,11 @@
     description = "digitaldata";
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [];
+
+    # Add your development machine's public SSH key here so you can log in
+    # without a password. Replace this placeholder with the actual key.
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILk4bC/9jWdrMGhuJTfIVpc+YyEULpFKaGQHIL2sRtV8 octantis"
+    ];
   };
 }
