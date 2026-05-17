6a438eccf2d9dc52fee4afb9498ff9f9db90ef72
DigitalData <legoxavierlocketravers@gmail.com>
2026-05-17 11:56:49 +1000

    Octantis / ssh config

--- hosts/octantis/configuration.nix
+++ hosts/octantis/configuration.nix
@@ -89,7 +89,9 @@
   #   enable = true;
   #   enableSSHSupport = true;
   # };
-  programs.ssh.startAgent = true;
+  programs.ssh = {
+    startAgent = true;
+  }
 
   # List services that you want to enable:
 
