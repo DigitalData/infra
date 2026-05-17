745662186bf4095add558870003f6d2977c5dc2b
DigitalData <legoxavierlocketravers@gmail.com>
2026-05-17 12:54:30 +1000

    Update auth_git.sh

--- scripts/auth_git.sh
+++ scripts/auth_git.sh
@@ -1,9 +1,10 @@
 
 USER="$(whoami)"
+CURRENT_DIR="$(dirname "$(realpath "$0")")"
 
 # if not exists, generate SSH key for git authentication
 if [ ! -f ~/.ssh/id_ed25519_git ]; then
-    echo "Generating git SSH Key for ${USER}@${HOSTNAME}"
+    echo "Generating git SSH Key for ${USER}@${HOSTNAME}:"
     ssh-keygen -t ed25519 -C "${USER}@${HOSTNAME}" -f ~/.ssh/id_ed25519_git
     echo "Please add the following public SSH key to git with write access:"
     cat ~/.ssh/id_ed25519_git.pub
@@ -11,10 +12,13 @@ fi
 
 # if agent not running, start it and add the SSH key
 if ! pgrep -x "ssh-agent" > /dev/null; then
-    echo "Starting ssh-agent and adding SSH key for git authentication"
+    echo "Starting ssh-agent and adding SSH key for git authentication:"
+    cd ~
     eval "$(ssh-agent -s)"
     ssh-add ~/.ssh/id_ed25519_git
 else
-    echo "ssh-agent is already running, adding SSH key for git authentication"
+    echo "ssh-agent is already running, adding SSH key for git authentication:"
     ssh-add -l | grep -q "id_ed25519_git" || ssh-add ~/.ssh/id_ed25519_git
 fi
+
+cd "${CURRENT_DIR}"
