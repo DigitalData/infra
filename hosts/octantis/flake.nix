dcb6a0e2af0491b1d1e7032aaf937bd264203d24
DigitalData <legoxavierlocketravers@gmail.com>
2026-05-22 11:03:44 +1000

    Try including disk config in host flake

--- hosts/octantis/flake.nix
+++ hosts/octantis/flake.nix
@@ -4,13 +4,17 @@
   inputs = {
     # Official NixOS package source, nixos-25.11 branch
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    disko.url = "github:nix-community/disko";
+    disko.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = { self, nixpkgs, disko, ... }@inputs: {
     nixosConfigurations.octantis = nixpkgs.lib.nixosSystem {
       modules = [
+        disko.nixosModules.disko
         # Import old configuration
-	./configuration.nix
+	      ./configuration.nix
+        ./disk.nix
       ];
     };
   };
