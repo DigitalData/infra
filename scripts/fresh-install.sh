b2a8530266c20729885d33c376d3c9057340513f
DigitalData <legoxavierlocketravers@gmail.com>
2026-05-22 12:03:18 +1000

    Enforce just giving a host name

--- scripts/fresh-install.sh
+++ scripts/fresh-install.sh
@@ -1,11 +1,6 @@
 set -e
 if [ -z "$1" ]; then
-  echo "Usage: $0 /hosts/<hostname>"
-  exit 1
-fi
-HOST_FOLDER="$1"
-if [ ! -e "$HOME/infra/$HOST_FOLDER" ]; then
-  echo "Error: $HOST_FOLDER is not a directory"
+  echo "Usage: $0 <hostname>"
   exit 1
 fi
 
@@ -15,17 +10,27 @@ if [ ! -e "$HOME/infra" ]; then
   git clone https://github.com/DigitalData/infra.git "$HOME/infra"
 fi
 
-echo "Running disko to destroy and format disk for new host..."
-sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko --flake "$HOME/infra/$HOST_FOLDER"
+HOST_NAME="$1"
+HOST_PATH="$HOME/infra/hosts/$HOST_NAME"
+if [ ! -e "$HOST_PATH" ]; then
+  echo "Error: $HOST_PATH is not a directory"
+  exit 1
+fi
+
+echo "Running disko for host folder: $HOST_PATH"
+nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode destroy "$HOST_PATH/disko.nix"
+
+echo "Disk destroyed and formatted. Now mounting..."
+nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode format,mount "$HOST_PATH/disko.nix"
 
 echo "Disk disko complete. Now generating NixOS configuration for new host..."
 sudo nixos-generate-config --no-filesystems --root /mnt
 
 echo "Generating NixOS configuration complete. Now copying hardware configuration to infra..."
-sudo cp -r /mnt/etc/nixos/hardware-configuration.nix "$HOME/infra/$HOST_FOLDER/"
+sudo cp -r /mnt/etc/nixos/hardware-configuration.nix "$HOST_PATH/hardware-configuration.nix"
 
 echo "Disk disko complete. Now installing NixOS to disk..."
-sudo nixos-install --root /mnt --no-root-passwd --flake "$HOME/infra/$HOST_FOLDER"
+sudo nixos-install --root /mnt --no-root-passwd --flake "$HOST_PATH#${HOST_NAME}"
 
 echo "Disk installation complete. Now setting root password for new host..."
 sudo nixos-enter --root /mnt -c 'passwd digitaldata'
