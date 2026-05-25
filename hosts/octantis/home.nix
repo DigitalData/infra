052eff4fa9b3a0ea025d329fc2a1e4ed51897a94
DigitalData <legoxavierlocketravers@gmail.com>
2026-05-26 00:29:02 +1000

    Octantis / Home - Add config ez cd (with correct multiline text?)

--- hosts/octantis/home.nix
+++ hosts/octantis/home.nix
@@ -21,11 +21,11 @@
     enable = true;
     initExtra = ''clear && neofetch'';
     shellAliases = {
-      "os-config" = '_BACK_DIR="$(dirname "$(realpath "$0")")" && cd /etc/nixos';
-      "os-back" = 'cd "$_BACK_DIR"';
-      "os-rebuild" = 'sudo nixos-rebuild switch --flake /etc/nixos/hosts/octantis';
-      "os-git-auth" = 'sh /etc/nixos/scripts/config/git-authenticate.sh';
-      "os-pull" = 'sh /etc/nixos/scripts/config/os-pull.sh';
+      "os-config" = ''_BACK_DIR="$(dirname "$(realpath "$0")")" && cd /etc/nixos'';
+      "os-back" = ''cd "$_BACK_DIR"'';
+      "os-rebuild" = ''sudo nixos-rebuild switch --flake /etc/nixos/hosts/octantis'';
+      "os-git-auth" = ''sh /etc/nixos/scripts/config/git-authenticate.sh'';
+      "os-pull" = ''sh /etc/nixos/scripts/config/os-pull.sh'';
     };
   };
   
