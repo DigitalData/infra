396997d157187ebec88fde7abae8ffc29f3831d5
DigitalData <legoxavierlocketravers@gmail.com>
2026-05-30 14:32:14 +1000

    Move flake to top-level using flake-parts as import

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Shared configurations for DigitalData hosts";
+
+  inputs = {
+    # Official NixOS package source, nixos-25.11 branch
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+
+    # Config modularization
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
+    # Disk partitioning and mounting configuration
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # User-level configuration
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{ nixpkgs, flake-parts, disko, home-manager, ... }: 
+    flake-parts.lib.mkFlake { inherit inputs; }
+    {
+      imports = [
+        ./hosts/octantis
+      ];
+    };
+}
