effd26487c428bf3149aa99684d089a08824b63d
DigitalData <legoxavierlocketravers@gmail.com>
2026-05-31 18:36:56 +1000

    Octantis / Use closed source NVIDIA driver for 960

--- hosts/octantis/hardware.nix
+++ hosts/octantis/hardware.nix
@@ -15,6 +15,7 @@
         modesetting.enable = true;
         powerManagement.enable = false;
         powerManagement.finegrained = false;
+        open = false;
         nvidiaSettings = true;
         package = config.boot.kernelPackages.nvidiaPackages.stable;
       };
