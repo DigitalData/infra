d21da613b0bfbeced6b0d4df9011e59b83b3ed89
DigitalData <legoxavierlocketravers@gmail.com>
2026-05-31 17:12:57 +1000

    Import from hosts now

--- flake.nix
+++ flake.nix
@@ -24,11 +24,5 @@
     };
   };
 
-  outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } 
-    {
-      imports = [
-        (inputs.import-tree ./modules)
-        ./hosts/octantis
-      ];
-    };
+  outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./modules ./hosts);
 }
