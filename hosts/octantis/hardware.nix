0b9e0d7a07ce03f9692850a882b5be0a90af0fc5
DigitalData <legoxavierlocketravers@gmail.com>
2026-05-31 18:32:58 +1000

    Octantis / NVIDIA / services typo

--- hosts/octantis/hardware.nix
+++ hosts/octantis/hardware.nix
@@ -8,15 +8,13 @@
     boot.kernelModules = [ "kvm-intel" ];
     boot.extraModulePackages = [ ];
 
-    service.xserver.videoDrivers = [ "nvidia" ];
-
+    services.xserver.videoDrivers = [ "nvidia" ];
     hardware = {
       nvidia-container-toolkit.enable = true;
       nvidia = {
         modesetting.enable = true;
         powerManagement.enable = false;
         powerManagement.finegrained = false;
-        open = false;
         nvidiaSettings = true;
         package = config.boot.kernelPackages.nvidiaPackages.stable;
       };
