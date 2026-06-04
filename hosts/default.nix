83f0adb1822c1786f2f709feec53e93d99688c38
DigitalData <legoxavierlocketravers@gmail.com>
2026-06-04 21:03:15 +1000

    Update default.nix

--- hosts/default.nix
+++ hosts/default.nix
@@ -9,7 +9,7 @@
     
     environment.variables = {
       # Set the default editor to Neovim
-      users = builtins.toString (builtins.attrNames config.home-manager.users);
+      HOME_USERS = builtins.toString (builtins.attrNames config.home-manager.users);
     };
 
     # Disable unfree packages by default, override in host configuration if necessary
