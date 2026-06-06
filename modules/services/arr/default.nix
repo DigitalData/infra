b0f7fa69d2ede3d20ef415e2c551700ce7599bad
DigitalData <legoxavierlocketravers@gmail.com>
2026-06-06 13:31:44 +1000

    Update default.nix

--- modules/services/arr/default.nix
+++ modules/services/arr/default.nix
@@ -1,7 +1,7 @@
 { self, inputs, ... }:
 {
   flake.modules.nixos.arr = { pkgs, ... }: {
-    services.jellyfin.enable = {
+    services.jellyfin = {
       enable = true;
       openFirewall = true;
     };
