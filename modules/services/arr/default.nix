767036494e0220af2725b53b37d74a85fa662763
DigitalData <legoxavierlocketravers@gmail.com>
2026-06-06 14:13:38 +1000

    Try adding a required mediaDir option for arr

--- modules/services/arr/default.nix
+++ modules/services/arr/default.nix
@@ -1,6 +1,13 @@
-{ self, inputs, ... }:
+{ self, inputs, lib, ... }:
 {
   flake.modules.nixos.arr = { pkgs, ... }: {
+
+    options.mediaDir = lib.mkOption {
+      type = lib.types.absolutePath;
+      required = true;
+      description = "Path to the arr media directory.";
+    };
+
     services.jellyfin = {
       enable = true;
       openFirewall = true;
