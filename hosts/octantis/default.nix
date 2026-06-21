81f71c701dbb69a3a5fc3580f6052d6ddcfdfbc7
DigitalData <legoxavierlocketravers@gmail.com>
2026-06-21 12:11:20 +1000

    Update default.nix

--- hosts/octantis/default.nix
+++ hosts/octantis/default.nix
@@ -28,7 +28,7 @@
     # condigure networking
     caddy.email = "legoxavierlocketravers+caddy@gmail.com";
     caddy.domain.internal = "octantis.local";
-    caddy.domain.external = "octantis.peccary-darter.ts.net"
+    caddy.domain.external = "octantis.peccary-darter.ts.net";
 
     # configure media
     media.arr = true;
